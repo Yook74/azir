9408e076d873b500876197175adb8295b01ccddd
Andrew Blomenberg <andrewBlomen@gmail.com>
2022-12-31 14:52:45 -0700

    Created a couple task endpoints and a helper decorator

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,2 +1,21 @@
+from functools import wraps
+
+from werkzeug.exceptions import NotFound
+
+
+def by_id(model_class):
+    def decorator(route):
+        @wraps(route)
+        def wrapper(id_):
+            row = model_class.query.filter_by(id=id_).first()
+            if row is None:
+                raise NotFound(f'No {model_class.__name__} found with the given ID')
+
+            return route(row)
+        return wrapper
+    return decorator
+
+
 from routes import device, operation, task
+
 blueprints = [device.bluep, operation.bluep, task.bluep]
--- routes/task.py
+++ routes/task.py
@@ -1,3 +1,20 @@
 from flask import Blueprint
 
+from models import *
+from routes import by_id
+
 bluep = Blueprint('task', __name__, url_prefix='/task/')
+
+
+@bluep.post('/<int:task_id>/complete')
+@by_id(Task)
+def complete_task(task: Task):
+    task.completed = True
+    db.session.commit()
+
+
+@bluep.post('/<int:task_id>/uncomplete')
+@by_id(Task)
+def uncomplete_task(task: Task):
+    task.completed = False
+    db.session.commit()
