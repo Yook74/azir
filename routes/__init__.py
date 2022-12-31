dbd1586d9c73ee7b06592ab6ed596e1c4a0defe5
Andrew Blomenberg <andrewBlomen@gmail.com>
2022-12-31 15:20:35 -0700

    Fixed by_id decorator bug

--- routes/__init__.py
+++ routes/__init__.py
@@ -6,8 +6,8 @@ from werkzeug.exceptions import NotFound
 def by_id(model_class):
     def decorator(route):
         @wraps(route)
-        def wrapper(id_):
-            row = model_class.query.filter_by(id=id_).first()
+        def wrapper(id):
+            row = model_class.query.filter_by(id=id).first()
             if row is None:
                 raise NotFound(f'No {model_class.__name__} found with the given ID')
 
