dbd4996c3524a431d3c132d6f674c4ed040d11a3
Andrew Blomenberg <andrewBlomen@gmail.com>
2022-12-31 18:53:36 -0700

    Added operation form and description pages

--- routes/operation.py
+++ routes/operation.py
@@ -1,3 +1,25 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+
+from routes import by_id
+from models import Operation, db
 
 bluep = Blueprint('operation', __name__, url_prefix='/operation/')
+
+
+@bluep.get('create')
+def operation_form():
+    return render_template('operation_form.html')
+
+
+@bluep.post('create')
+def new_operation():
+    db.session.add(Operation(**{key: request.form[key] for key in ('description', 'name')}))
+    db.session.commit()
+
+    return operation_form()
+
+
+@bluep.get('<int:id>')
+@by_id(Operation)
+def operation_detail(operation):
+    return render_template('operation_description.html', operation=operation)
